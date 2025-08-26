cbe10866c1e16c23cb42494a1bfd4091f6c1ea87
Andrew <andrew@Andrews-iMac.lan>
2025-08-26 11:34:08 -0500

    Additions to the main window and start label creation

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,11 +1,13 @@
 # used for the main window
 import tkinter as tk
+from tkinter import ttk
 
 class MainWindow:
     # root window properties
     def __init__(self):
         self.root = tk.Tk()
-        height, width = 1100, 800
+        self.root.configure(background = "#222222")
+        height, width = 400, 600
         # get height and width of screen
         window_height = self.root.winfo_screenheight()
         window_width = self.root.winfo_screenwidth()
